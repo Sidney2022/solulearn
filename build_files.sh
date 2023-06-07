7eefb3d1621b670962f6463e8670e0cf1f41b752
Sidney Uwaya <uwayasidney@gmail.com>
2023-06-07 18:20:49 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.6 -m pip install -r requirements.txt
+python3.6 manage.py collectstatic
+python3.6 manage.py makemigrations --noinput
+python3.6 manage.py migrate --noinput
