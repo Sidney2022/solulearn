65aa4d140ab33149b66c2ac937b61ccd5b006dae
Sidney2022 <uwayasidney@gmail.com>
2023-08-16 23:43:58 +0100

    media

--- solulearn/settings.py
+++ solulearn/settings.py
@@ -146,8 +146,8 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_ROOT = '/home/Solulearn/solulearn/media/'
-MEDIA_ROOT = 'C:/Users/USER/desktop/proj/solulearn/media/'
+MEDIA_ROOT = '/home/Solulearn/solulearn/media/'
+# MEDIA_ROOT = 'C:/Users/USER/desktop/proj/solulearn/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
