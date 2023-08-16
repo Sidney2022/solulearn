3188a64a835336e99081a1437a50448ed6c0a558
Sidney2022 <uwayasidney@gmail.com>
2023-08-16 22:55:18 +0100

    update

--- build_files.sh
+++
@@ -1,4 +0,0 @@
-python3.6 -m pip install -r requirements.txt
-python3.6 manage.py collectstatic
-python3.6 manage.py makemigrations --noinput
-python3.6 manage.py migrate --noinput
--- code.py
+++
@@ -1,3 +0,0 @@
-print("TE XT ".lower().strip())
-import random
-print(random.randint(0, 999999))
