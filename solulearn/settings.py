734120ee492b8c2c4b9cdd06f146d7ba6aee8772
Sidney2022 <uwayasidney@gmail.com>
2023-08-16 23:48:03 +0100

    media

--- solulearn/settings.py
+++ solulearn/settings.py
@@ -146,7 +146,7 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = '/home/Solulearn/solulearn/media/'
+MEDIA_ROOT = '/home/solulearn/solulearn/media/'
 # MEDIA_ROOT = 'C:/Users/USER/desktop/proj/solulearn/media/'
 
 # Default primary key field type
